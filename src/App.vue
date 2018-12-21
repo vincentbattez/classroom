fd75696c752234f9c67427b701457def75372b04
Vincent Battez <vincent.battez.pro@gmail.com>
2018-12-22 00:14:41 +0100

    feat(sidebar)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
+    <Sidebar />
     <Navbar />
     <router-view :key="$route.fullPath"/>
   </div>
 </template>
 
 <script>
+import Sidebar from '@/components/Sidebar.vue'
 import Navbar from '@/components/Navbar.vue'
 export default {
-  components: { Navbar }
+  components: { Sidebar, Navbar }
 }
 </script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="sidebar">
+    <ul>
+      <li class="logo">
+        <router-link :to="{ name: 'dashboard' }" class="link">
+          <BaseIcon name="logo" title="Logo"/>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'dashboard' }" class="link active">
+          <BaseIcon name="dashboard" title="Dashboard"/>
+          <span>Dashboard</span>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'dashboard' }" class="link">
+          <BaseIcon name="classroom" title="Classroom"/>
+          <span>Classe 2<sup>nd</sup>3</span>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
